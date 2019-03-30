7e92507bad2a34625f15417a6c7cb37e327f910b
Frosti G <frg17@hi.is>
2019-03-30 13:01:42 +0000

    change

---
+++ external/fgutils/src/include/fgutils/fileio.h
@@ -0,0 +1,14 @@
+#ifndef __fileio_h__
+#define __fileio_h__
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+const char *fgReadFile(const char *filename);
+
+#ifdef __cplusplus
+    }
+#endif
+
+#endif
